cd5282992f68aa33e756d4d45ed57a156f672375
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-09-24 22:21:25 -0300

    Exemplo POST

---
+++ exemplos-spring1/src/main/java/br/senac/tads/dsw/exemplosspring1/FormularioController.java
@@ -0,0 +1,48 @@
+package br.senac.tads.dsw.exemplosspring1;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/formulario")
+public class FormularioController {
+
+	@GetMapping
+	public ModelAndView abrirFormulario() {
+		ModelAndView mv = new ModelAndView("formulario");
+		DadosPessoais dados = new DadosPessoais();
+		dados.setId(999L);
+		dados.setNome("Zezinho Silveira");
+		dados.setEmail("zezinho@email.com");
+		dados.setSenha("abcd1234");
+		dados.setSenhaRepetida("abcd1234");
+		dados.setDtNascimento(LocalDate.of(1998, 9, 30));
+		dados.setSexo(1);
+		dados.setNumeroSorte(72);
+		dados.setPeso(BigDecimal.valueOf(90.7));
+		dados.setAltura(BigDecimal.valueOf(1.72));
+		dados.setDescricao("bla bla bla");
+		dados.setInteresses(new String[] { "Tecnologia", "Viagens" });
+		mv.addObject("dadosPessoais", dados);
+		return mv;
+	}
+
+	@PostMapping("/salvar")
+	public ModelAndView salvar(
+			@ModelAttribute("dadosPessoais") DadosPessoais dadosRecebidos) {
+		ModelAndView mv = new ModelAndView("resultado-formulario");
+		mv.addObject("dados", dadosRecebidos);
+		return mv;
+	}
+	
+	
+	
+
+}
